594adf94fd861cf3570807de09b7dbf457a5b1af
Mark Rivers <rivers@cars.uchicago.edu>
2019-12-15 17:01:50 -0600

    Add readInt32

--- GenICamApp/src/ADGenICam.h
+++ GenICamApp/src/ADGenICam.h
@@ -22,6 +22,7 @@ public:
 
     // virtual methods to override from ADDriver
     virtual asynStatus writeInt32( asynUser *pasynUser, epicsInt32 value);
+    virtual asynStatus readInt32( asynUser *pasynUser, epicsInt32 *value);
     virtual asynStatus writeFloat64( asynUser *pasynUser, epicsFloat64 value);
     //virtual asynStatus writeOctet(asynUser *pasynUser, const char *value,
     //                              size_t nChars, size_t *nActual);
