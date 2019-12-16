90bb7c3a2e831f3badf47ee53fe1d9822d7e7ada
Mark Rivers <rivers@cars.uchicago.edu>
2019-12-16 16:30:26 -0600

    Added example for generating either int64in/int64out or ai/ao records

--- addCamera.sh
+++ addCamera.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Use this line for int64out and int64in records.  Requires EPICS base 3.16.1 or later, or EPICS 7.
+#python scripts/makeDb.py xml/$1.xml --devInt64 GenICamApp/Db/$1.template
+
+# Use this line for longout and longin records on older versions of EPICS base. 
+# Limits GenICam Integer features to 52 bits because ao records with 64-bit floats are used.
 python scripts/makeDb.py xml/$1.xml GenICamApp/Db/$1.template
+
 python scripts/makeAdl.py xml/$1.xml GenICamApp/op/adl/$1
