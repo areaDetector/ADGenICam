fd488253e420f1be5b25b162d975b31ad88ff07f
Mark Rivers <rivers@cars.uchicago.edu>
2020-01-16 18:09:13 -0600

    Specify to use Python3

--- addCamera.sh
+++ addCamera.sh
@@ -5,6 +5,6 @@
 
 # Use this line for ao and ai records on older versions of EPICS base. 
 # Limits GenICam Integer features to 52 bits because ao and ai records with 64-bit floats are used.
-python scripts/makeDb.py xml/$1.xml GenICamApp/Db/$1.template
+python3 scripts/makeDb.py xml/$1.xml GenICamApp/Db/$1.template
 
-python scripts/makeAdl.py xml/$1.xml GenICamApp/op/adl/$1
+python3 scripts/makeAdl.py xml/$1.xml GenICamApp/op/adl/$1
