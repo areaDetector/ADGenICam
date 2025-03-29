0709c15d99767bb2afc51bbf2ed142246d4716dd
Mark Rivers <rivers@cars.uchicago.edu>
2025-03-29 15:50:53 -0500

    Add 2 more arguments to pass to Python scripts for optional prefix

--- addCamera.sh
+++ addCamera.sh
@@ -4,10 +4,10 @@
 # On RHEL7 I use "conda activate base" to get a working version.
 
 # Use this line for int64out and int64in records.  Requires EPICS base 3.16.1 or later, or EPICS 7.
-#python scripts/makeDb.py xml/$1.xml --devInt64 GenICamApp/Db/$1.template
+#python scripts/makeDb.py xml/$1.xml --devInt64 GenICamApp/Db/$1.template $2 $3
 
 # Use this line for ao and ai records on older versions of EPICS base. 
 # Limits GenICam Integer features to 52 bits because ao and ai records with 64-bit floats are used.
-python3 scripts/makeDb.py xml/$1.xml GenICamApp/Db/$1.template
+python3 scripts/makeDb.py xml/$1.xml GenICamApp/Db/$1.template $2 $3
 
-python3 scripts/makeAdl.py xml/$1.xml GenICamApp/op/adl/$1
+python3 scripts/makeAdl.py xml/$1.xml GenICamApp/op/adl/$1 $2 $3
